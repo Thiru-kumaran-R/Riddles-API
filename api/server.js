363d3bf25dbab5a2cd42ee587aff9ca6c3ba0a3c
thirukumaran05 <learningthiru@gmail.com>
2024-05-27 16:45:31 +0530

    config

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes/routes.js";
 
@@ -8,7 +7,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const DB = process.env.RIDDLES_DB;
-app.use(bodyParser.json());
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -26,15 +25,17 @@ app.use((req, res, next) => {
     message: "No such route exits",
     status: 404,
   });
-  next();
 });
 
 app.use((error, req, res, next) => {
-  const message = error.message;
-  const status = error.status;
+  const message = error.message || 'An unknown error occurred';
+  const status = error.status || 500 ;
   return res.json({ message: message, status: status });
 });
 
 app.listen(PORT, () => {
-  mongoose.connect(DB).then(() => console.log(`port running at ${PORT}`));
+  mongoose
+    .connect(DB)
+    .then(() => console.log(`port running at ${PORT}`))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
 });
